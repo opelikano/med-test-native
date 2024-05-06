99b03a9a57b2729e813f2bf9fbfbd37153f1c19f
andrii <modxer@gmail.com>
2024-05-07 02:01:07 +0300

    revert_commit

--- database/Sql.php
+++ database/Sql.php
@@ -8,7 +8,7 @@ class Sql {
 
     private $host = 'localhost';
     private $user = 'root';
-    private $database = 'laravel_1';
+    private $database = '';
     private $password = '';
 
     private function __construct() {
